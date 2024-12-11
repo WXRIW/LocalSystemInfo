7e464b61bb4a7048ad59c2ef34dc1cf4559d5d5e
XY Wang <wxy11787@foxmail.com>
2024-12-11 22:38:56 +0800

    Add descriptions for methods

--- Client/Socket.h
+++ Client/Socket.h
@@ -37,12 +37,27 @@ private:
 public:
 	Socket() : socketId(-1), port(12345) {}
 
+	/// <summary>
+	/// 初始化 Socket
+	/// </summary>
+	/// <returns>初始化是否成功</returns>
 	bool Init();
 
+	/// <summary>
+	/// 设置发送端口
+	/// </summary>
+	/// <param name="port">端口号</param>
 	void SetPort(short port);
 
+	/// <summary>
+	/// 广播信息
+	/// </summary>
+	/// <param name="data">要广播的信息</param>
 	void Broadcast(std::string data) const;
 
+	/// <summary>
+	/// 关闭 Socket
+	/// </summary>
 	void Close() const;
 };
 
